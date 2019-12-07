c4205def8b131ebde0ec3983a178705f1942f0e6
dudl2547 <tonydudley23@gmail.com>
2019-12-07 00:53:49 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {}
+variable "vpc_cidr" {}
+variable "subnet_cidr" {}
+variable "key_name" {}
+variable "public_key_path" {}
+variable "instance_type" {}
+variable "ami" {}
+variable "access_key" {}
+variable "secret_key" {}
